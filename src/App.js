3edce636e179eb59c1ea545d43c24d88d84f43e4
rick1e <alrickab@gmail.com>
2022-02-21 10:05:37 -0500

    MVP for flip up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [board, setBoard] = useState([0, 0, 0,
+    0, 0, 0,
+    0, 0, 0]);
+
+  const checkComplete = () => {
+    let result = board.reduce((total, cell) => {
+      return total + cell
+    }, 0)
+
+    return result === 0 || result === board.length
+  }
+
+  const flip = (index) => {
+    if (!checkComplete()) {
+      const cell = document.getElementById(index)
+      let newBoard = board;
+      newBoard[index] = (newBoard[index] + 1) % 2
+      cell.className = newBoard[index] === 0 ? "cell" : "cell blue"
+      setBoard(newBoard)
+      console.log(newBoard)
+    }
+
+  }
+  const drawBoard = () => {
+    const boardDivs = board.map((cell, index) => {
+      let cellStyle = cell === 0 ? "cell" : "cell blue"
+      return <div key={index} id={index} className={cellStyle} onClick={() => { flip(index) }}></div>
+    })
+
+    return boardDivs;
+  }
+
+  const getStartIndicies = (size) => {
+    let indexList = []
+    while (indexList.length < size / 2 - 1) {
+      let possibleIndex = Math.floor(Math.random() * size)
+      if (!indexList.includes(possibleIndex)) {
+        indexList.push(possibleIndex)
+      }
+    }
+
+    return indexList
+  }
+
+  const startGame = () => {
+    let count = 0
+    const startButton = document.getElementById("startButton")
+    startButton.disabled = true;
+    let startInicies = getStartIndicies(board.length)
+    let newBoard = board
+
+    console.log(startInicies)
+
+    for (let index = 0; index < startInicies.length; index++) {
+      const cell = document.getElementById(startInicies[index])
+      newBoard[startInicies[index]] = (newBoard[startInicies[index]] + 1) % 2
+      cell.className = newBoard[startInicies[index]] === 0 ? "cell" : "cell blue"
+      setBoard(newBoard)
+    }
+
+    let AI = setInterval(() => {
+      let index = Math.floor(Math.random() * board.length)
+      if (checkComplete()) {
+        clearInterval(AI)
+        const startButton = document.getElementById("startButton")
+        startButton.disabled = false;
+        return
+      }
+      flip(index)
+      console.log(count++)
+    }, 500)
+
+  }
+
+
+  useEffect(() => {
+
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="board">
+        {drawBoard()}
+      </div>
+      <button id="startButton" onClick={startGame}>
+        Start
+      </button>
+
+
     </div>
   );
 }
